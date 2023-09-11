a4d7602bfe3b9b539d7fad59c2894578f4de593b
ChadhaH <chadha.hamed1@gmail.com>
2023-09-11 15:51:50 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,9 @@
 def divisible_by_2(my_list=[]):
     multi = []
     for i in range(len(my_list)):
-        if (my_list[i]) % 2 == 0:
+        if my_list[i] % 2 == 0:
             multi.append(True)
         else:
             multi.append(False)
-    retunr (multi)
+
+    return (multi)
